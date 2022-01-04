877671db303783c59d8da0bade7b17f0338df2a4
MiguelTolino <mmateo-t@student.42madrid.com>
2022-01-04 17:51:13 +0100

    ^C fixed

--- files/srcs/signals.c
+++ files/srcs/signals.c
@@ -6,17 +6,17 @@
 /*   By: mmateo-t <mmateo-t@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/17 12:22:42 by mmateo-t          #+#    #+#             */
-/*   Updated: 2022/01/03 13:39:41 by mmateo-t         ###   ########.fr       */
+/*   Updated: 2022/01/04 17:50:51 by mmateo-t         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-//FIXME: ^C still appears in command line
-
 void sigint_handler(int sig)
 {
 	rl_replace_line("", 0);
+	ft_putstr_fd("\b\b", STDOUT_FILENO);
+	ft_putstr_fd("  ", STDOUT_FILENO);
 	ft_putchar_fd('\n', STDOUT_FILENO);
 	rl_on_new_line();
 	rl_redisplay();
