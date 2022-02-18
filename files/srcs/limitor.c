980455ff98a6777130dad3ffde3bb6a206108390
mmateo-t@student.42madrid.com <mmateo-t@c1r13s5.42madrid.com>
2022-02-18 19:45:45 +0100

    new commit

--- files/srcs/limitor.c
+++ files/srcs/limitor.c
@@ -6,7 +6,7 @@
 /*   By: mmateo-t <mmateo-t@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/08 19:28:38 by mmateo-t          #+#    #+#             */
-/*   Updated: 2022/02/18 18:31:04 by mmateo-t         ###   ########.fr       */
+/*   Updated: 2022/02/18 19:44:28 by mmateo-t         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,14 +26,6 @@ int	limitor_function(t_token *limit)
 	int fd;
 	int stop;
 	char *str;
-<<<<<<< HEAD
-	char buff[1028];
-	fd = 5;
-	
-	(void)str;
-	(void)fd;
-	while (read(STDIN_FILENO, buff, ft_strlen(limit)) > 0)
-=======
 
 	stop = 0;
 	fd = open("heredoc.tmp", O_WRONLY | O_CREAT | O_APPEND, 0644);
@@ -43,7 +35,6 @@ int	limitor_function(t_token *limit)
 		return (1);
 	}
 	while (!stop)
->>>>>>> 6df98fb76b4d94e38136069c16fcfc159035287d
 	{
 		signal(SIGINT, &sigint_heredoc); //FIXME: Solucionar ^C
 		str = readline("heredoc > ");
@@ -65,24 +56,3 @@ int	limitor_function(t_token *limit)
 	}
 	return (0);
 }
-<<<<<<< HEAD
-
-int	limitor_function(char *limit)
-{
-	int fd;
-	char buff[16];
-	int bytes;
-
-	(void)bytes;
-	fd = open("LIMITOR.txt", O_RDWR);
-	if (fd < 0)
-		throw_error("Error in fd");
-	while (!exists_limitor(fd, limit))
-	{
-		bytes = read(STDIN_FILENO, buff, 16);
-		ft_putstr_fd(buff, fd);
-	}
-	return (fd);
-}
-=======
->>>>>>> 6df98fb76b4d94e38136069c16fcfc159035287d
