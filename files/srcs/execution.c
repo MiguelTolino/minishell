094baf1080f811a2a43ab4010d3515711aa7aa5e
MiguelTolino <mmateo-t@student.42madrid.com>
2022-02-22 19:19:17 +0100

    fixed pipes execution but need to test

--- files/srcs/execution.c
+++ files/srcs/execution.c
@@ -6,7 +6,7 @@
 /*   By: mmateo-t <mmateo-t@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/10 18:08:02 by mmateo-t          #+#    #+#             */
-/*   Updated: 2022/02/21 23:41:42 by mmateo-t         ###   ########.fr       */
+/*   Updated: 2022/02/22 19:16:38 by mmateo-t         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 //FIXME: Me crea varios procesos y no se cierra
 //FIXME: Aprender a usar kill para eliminiar procesos abiertos
 
-void exec_pipe(t_list *cmdlist)
+int exec_pipe(t_list *cmdlist)
 {
 	int p[2];
 	pid_t pid;
@@ -29,28 +29,32 @@ void exec_pipe(t_list *cmdlist)
 	{
 		cmd = ((t_cmd_data *)cmdlist->content)->exec_cmd;
 		if (pipe(p) < 0)
-			throw_error("Pipe Error:");
+			return (throw_error("Pipe Error"));
 		if ((pid = fork()) < 0)
-			throw_error("Fork Error:");
+			return (throw_error("Fork Error"));
 		else if (!pid)
 		{
 			dup2(fd_in, STDIN_FILENO); //change the input according to the old one
 			if (cmdlist->next != NULL)
 				dup2(p[WRITE_END], STDOUT_FILENO);
 			close(p[READ_END]);
-			check_path(&cmd[0]);
-			execve(cmd[0], cmd, global.env);
-			throw_error("Execution Error:");
+			if (!exec_builtins(cmd))
+			{
+				check_path(&cmd[0]);
+				execve(cmd[0], cmd, global.env);
+			}
+			exit(global.exit_status);
+			return (throw_error("Execution Error"));
 		}
 		else
 		{
-			wait(&global.exit_status);
-			wait(NULL);
+			waitpid(pid, &global.exit_status, 0);
 			close(p[WRITE_END]);
 			fd_in = p[READ_END]; //save the input for the next command
 			cmdlist = cmdlist->next;
 		}
 	}
+	return (0);
 }
 
 int exec_simple(char **cmds)
@@ -76,9 +80,9 @@ int execution(t_shell *shell)
 	t_cmd_data *data;
 
 	data = (t_cmd_data *)shell->cmdlist->content;
-/* 	if (ft_lstsize(shell->cmdlist) > 1)
+ 	if (ft_lstsize(shell->cmdlist) > 1)
 		exec_pipe(shell->cmdlist);
-	else */
+	else
 	{
 		if (!exec_builtins(data->exec_cmd))
 		{
